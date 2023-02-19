748df666c52b89c0108804873ee259bb6b73f6ee
John Hurliman <jhurliman@jhurliman.org>
2023-02-18 20:46:18 -0800

    Fix isolatedModules incompatibility, smaller compression allocations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,44 +1,46 @@
-export const enum ZfpType {
-  None = 0,
-  Int32 = 1,
-  Int64 = 2,
-  Float = 3,
-  Double = 4,
+declare module "wasm-zfp" {
+  export const ZfpType: {
+    INT32: 1;
+    INT64: 2;
+    FLOAT: 3;
+    DOUBLE: 4;
+  };
+
+  export type ZfpBuffer = number;
+
+  export type ZfpInput = {
+    data: Int32Array | BigInt64Array | Float32Array | Float64Array;
+    shape: [number, number, number, number];
+    strides: [number, number, number, number];
+    dimensions: number;
+  };
+
+  export type ZfpCompressOptions = {
+    tolerance?: number;
+    rate?: number;
+    precision?: number;
+  };
+
+  export type ZfpResult = {
+    data: Int32Array | BigInt64Array | Float32Array | Float64Array;
+    dataPointer: number;
+    bufferSize: number;
+    size: number;
+    scalarSize: number;
+    shape: [number, number, number, number];
+    strides: [number, number, number, number];
+    dimensions: number;
+    type: 1 | 2 | 3 | 4;
+  };
+
+  type Zfp = {
+    isLoaded: Promise<void>;
+    createBuffer: () => number;
+    freeBuffer: (zfpBuffer: number) => void;
+    compress: (zfpBuffer: number, input: ZfpInput, options?: ZfpCompressOptions) => ZfpResult;
+    decompress: (zfpBuffer: number, src: Uint8Array) => ZfpResult;
+  };
+
+  const zfp: Zfp;
+  export default zfp;
 }
-
-export type ZfpBuffer = number;
-
-export type ZfpInput = {
-  data: Int32Array | BigInt64Array | Float32Array | Float64Array;
-  shape: [number, number, number, number];
-  strides: [number, number, number, number];
-  dimensions: number;
-}
-
-export type ZfpCompressOptions = {
-  tolerance?: number;
-  rate?: number;
-  precision?: number;
-}
-
-export type ZfpResult = {
-  data: Int32Array | BigInt64Array | Float32Array | Float64Array;
-  dataPointer: number;
-  bufferSize: number;
-  size: number;
-  scalarSize: number;
-  shape: [number, number, number, number];
-  strides: [number, number, number, number];
-  dimensions: number;
-  type: ZfpType;
-};
-
-export type Zfp = {
-  isLoaded: Promise<void>;
-  createBuffer: () => number;
-  freeBuffer: (zfpBuffer: number) => void;
-  compress: (zfpBuffer: number, input: ZfpInput, options?: ZfpCompressOptions) => ZfpResult;
-  decompress: (zfpBuffer: number, src: Uint8Array) => ZfpResult;
-};
-
-export default Zfp;
